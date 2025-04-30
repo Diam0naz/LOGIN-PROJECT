b0f110112fcb47509c30ec3fdf245169950f1049
Diam0naz <jutenoh@gmail.com>
2025-04-30 17:39:04 +0000

    Adds projects

---
+++ dist/scripts.js
@@ -0,0 +1,108 @@
+let btn = document.querySelector("button[type=submit]");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let isValid = true;
+  let fullName = document.querySelector("input[type=text]").value;
+  let email = document.querySelector("input[type=email]").value;
+  let birthDate = document.querySelector("input[type=date]").value;
+  let passWord = document.querySelector("input[type=password]").value;
+
+  if (!fullName || fullName == "") {
+    isValid = false;
+    let span = document.createElement("span");
+    span.style.color = "#ff0000";
+    span.style.fontSize = "10px";
+    span.style.margin = "0px";
+    span.style.padding = "0px";
+    span.innerText = "Fullname cannot be empty";
+    document.getElementById("name").appendChild(span);
+    setTimeout(() => {
+      span.remove();
+    }, 6000);
+  }
+
+  if (!birthDate || birthDate == "") {
+    isValid = false;
+    let span = document.createElement("span");
+    span.style.color = "#ff0000";
+    span.style.fontSize = "10px";
+    span.style.margin = "0px";
+    span.style.padding = "0px";
+    span.innerText = "Date Of Birth cannot be empty";
+    document.getElementById("dOb").appendChild(span);
+    setTimeout(() => {
+      span.remove();
+    }, 6000);
+  }
+
+  if (!email || email == "") {
+    isValid = false;
+    let span = document.createElement("span");
+    span.style.color = "#ff0000";
+    span.style.fontSize = "10px";
+    span.style.margin = "0px";
+    span.style.padding = "0px";
+    span.innerText = "Email cannot be empty";
+    document.getElementById("email").appendChild(span);
+    setTimeout(() => {
+      span.remove();
+    }, 6000);
+  }
+
+  if (!passWord || passWord == "") {
+    isValid = false;
+    let span = document.createElement("span");
+    span.style.color = "#ff0000";
+    span.style.fontSize = "10px";
+    span.style.margin = "0px";
+    span.style.padding = "0px";
+    span.innerText = "Password cannot be empty";
+    document.getElementById("password").appendChild(span);
+    setTimeout(() => {
+      span.remove();
+    }, 6000);
+  } else {
+    if (passWord.length < 8) {
+      isValid = false;
+      let span = document.createElement("span");
+      span.style.color = "#ff0000";
+      span.style.fontSize = "10px";
+      span.style.margin = "0px";
+      span.style.padding = "0px";
+      span.innerText = "Password cannot be less than 8";
+      document.getElementById("password").appendChild(span);
+      setTimeout(() => {
+        span.remove();
+      }, 6000);
+    }
+  }
+
+  if (isValid) {
+    let news = "Registration successful!";
+    alert(news);
+  } else {
+    let warn = "Please fill all fields";
+    alert(warn);
+    document.querySelector("form").reset();
+  }
+});
+
+let showPass = document.querySelector("#showPass");
+showPass.addEventListener("click", (e) => {
+  e.preventDefault();
+  let password = document.querySelector("input[name=password]");
+  if (password.type == "password") {
+    showPass.innerText = "Hide Password";
+    password.type = "text";
+  } else {
+    showPass.innerText = "Show Password";
+    password.type = "password";
+  }
+});
+
+let resetForm = document.querySelector("button[type=reset]");
+resetForm.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("form").reset();
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
- /** @type {import('tailwindcss').Config} */
- export default {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'playWriteCuba':  '"Playwrite CU", "cursive";',
+      'raleWay': '"Raleway", "sans-serif";'
+    },
+    extend: {
+      animation: {
+        "spin-slow": "spin 6s linear infinite;",
+        glow: "box-shadow: 0 0 20px rgba(255, 0, 255, 0.5), 0 0 40px rgba(0, 0, 255, 0.3)",
+        "pulse-slow": "pulse 5s ease-in-out infinite reverse",
+        ripple: "ripple 6s linear infinite alternate",
+      },
+      keyframes: {
+        "spin-slow": {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        ripple: {
+          "0%": { transform: "scale(1)", opacity: "1" },
+          "100%": { transform: "scale(1.5)", opacity: "0" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
